f9ead8eab524a319c80bfe04024a5d44209a5bda
Owais A. Khan <owais.khan@cr-pl.com>
2020-03-18 11:29:22 +0500

    animations

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -8,16 +8,16 @@ import {trigger, animate, style, group, animateChild, query, stagger, transition
   animations : [trigger('routerTransition', [
     transition('* <=> *', [
       /* order */
-      /* 1 */ query(':enter, :leave', style({ opacity : 0 ,  position: 'fixed', width:'100%' })
+      /* 1 */ query(':enter, :leave', style({ opacity : 0 ,  position: 'fixed', width:'75%' })
         , { optional: true }),
       /* 2 */ group([  // block executes in parallel
         query(':enter', [
           style({ transform: 'translateX(100%)' , opacity : 0 }),
-          animate('0.9s ease-in-out', style({ transform: 'translateX(0%)' , opacity: 1 }))
+          animate('0.7s ease-in-out', style({ transform: 'translateX(0%)' , width:'75%', opacity: 1 }))
         ], { optional: true }),
         query(':leave', [
           style({ transform: 'translateX(0%)' , opacity:1 }),
-          animate('0.9s ease-in-out', style({ transform: 'translateX(-100%)' , opacity: 0 }))
+          animate('0.7s ease-in-out', style({ transform: 'translateX(-100%)' , width:'75%', opacity: 0 }))
         ], { optional: true }),
       ])
     ])
