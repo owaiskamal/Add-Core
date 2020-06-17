077bc91c8d78db63e062289855e9fad52b8bc1c6
Owais A. Khan <owais.khan@cr-pl.com>
2020-06-17 12:42:47 +0500

    BENACNO-verify

--- src/app/dynamic-form-builder/atoms/textbox.ts
+++ src/app/dynamic-form-builder/atoms/textbox.ts
@@ -1,19 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
-// text,email,tel,textarea,password, 
+// text,email,tel,textarea,password,
 @Component({
     selector: 'textbox',
     template: `
       <div [formGroup]="form">
-       
+
         <!-- input class="form-control"  [id]="field?.name" [name]="field?.ColumnName" [formControlName]="field?.ColumnName" [placeholder]= "field.ColumnLabel"> -->
         <!-- <textarea *ngIf="field.multiline"  [class.is-invalid]="isDirty && !isValid" [formControlName]="field.name" [id]="field.name"
-         class="form-control" [placeholder]="field.placeholder"></textarea> --> 
-        
-           <input type="text" style="font-weight:500;"  [formControlName]="field?.ColumnName" [placeholder]= "field.ColumnLabel" pInputText>
-          
-      </div> 
+         class="form-control" [placeholder]="field.placeholder"></textarea> -->
+
+           <input type="text" style="font-weight:500;" [attr.maxLength]='field.MaxLeng'  [formControlName]="field?.ColumnName" [placeholder]= "field.ColumnLabel" pInputText>
+
+      </div>
     `
 })
 export class TextBoxComponent {
@@ -21,9 +21,8 @@ export class TextBoxComponent {
     @Input() form:FormGroup;
     get isValid() { return this.form.controls[this.field.ColumnName].valid; }
     get isDirty() { return this.form.controls[this.field.ColumnName].dirty; }
-  
+
     constructor() {
 
     }
   }
-    
--- src/app/dynamic-form-builder/atoms/textboxnum.ts
+++ src/app/dynamic-form-builder/atoms/textboxnum.ts
@@ -1,19 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
-// text,email,tel,textarea,password, 
+// text,email,tel,textarea,password,
 @Component({
     selector: 'textboxnum',
     template: `
-      <div [formGroup]="form">
-       
-        <p-inputNumber *ngIf="field.DataType == 'N'" style="font-weight:500;"  [name]="field?.ColumnName" [formControlName]="field?.ColumnName" [placeholder]= "field.ColumnLabel"></p-inputNumber>
+      <div class="p-fluid p-formgrid p-grid" [formGroup]="form">
+
+      <div  class="p-field p-col">
+        <p-inputNumber class="p-field p-col" *ngIf="field.DataType == 'N'" style="font-weight:500;"  [name]="field?.ColumnName" [formControlName]="field?.ColumnName" [placeholder]= "field.ColumnLabel"></p-inputNumber>
+        </div>
         <p-inputNumber *ngIf="field.DataType == 'C'" style="font-weight:500;" [formControlName]="field.ColumnName"  mode="currency" currency="PKR" locale="en-IN"></p-inputNumber>
+     <div  class="p-field p-col">
+        <button  pButton *ngIf="field.ISBTNVERIFY == 'Y'"  style="font-weight:900;" type="button" label="verify" class="ui-button-rounded  ui-button-warning"></button>
+</div>
         <!-- <textarea *ngIf="field.multiline"  [class.is-invalid]="isDirty && !isValid" [formControlName]="field.name" [id]="field.name"
          class="form-control" [placeholder]="field.placeholder"></textarea> -->
          <!-- <input class="form-control"  [id]="field?.name" [name]="field?.ColumnName" [formControlName]="field?.ColumnName" [placeholder]= "field.ColumnLabel"> -->
-      </div> 
-      
+      </div>
+
 
     `
 })
@@ -22,9 +27,8 @@ export class TextBoxComponentNum {
     @Input() form:FormGroup;
     get isValid() { return this.form.controls[this.field.ColumnName].valid; }
     get isDirty() { return this.form.controls[this.field.ColumnName].dirty; }
-  
+
     constructor() {
 
     }
   }
-    
