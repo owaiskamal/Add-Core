e2fae81f1cc88ee67825ffae55675d411dcfd95d
Owais A. Khan <owais.khan@cr-pl.com>
2020-09-04 10:58:54 +0500

    pre-select trans-checker

--- src/app/frm-trans-stage/frm-trans-stage.component.ts
+++ src/app/frm-trans-stage/frm-trans-stage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import {TransStageService} from "./../trans-stage.service";
+import { element } from 'protractor';
 @Component({
   selector: 'app-frm-trans-stage',
   templateUrl: './frm-trans-stage.component.html',
@@ -43,29 +44,47 @@ export class FrmTransStageComponent implements OnInit {
     this.filesMaster = res;
     this.files = Object.keys(res);
 
+
     console.log(this.files,"filesnames");
     this.filesDropdown = this.files.map((o) => ({
       name : o,
       value : o
       }))
-      console.log(this.filesDropdown,"raw data"); 
-      
+      console.log(this.filesDropdown,"raw data");
+       this.selectedCity =this.filesDropdown[0];
+       console.log(this.selectedCity , "pre-select");
+      this.selectedFile(this.selectedCity , this.opt);
     })
   }
   selectedFile(event,element){
    element.hide();
    this.filesArray =[]
-   var selectedFile = event.value.name;
+   var selectedFile = ""
+   if(event.value.name == undefined)
+   {
+
+    selectedFile = event['value'];
+    console.log(event['value'] , "this is pre-selected ");
+   }
+   else
+   {
+    selectedFile = event.value.name;
+    console.log(selectedFile , "this is event ");
+   }
+
    var filesArray = []
   filesArray = this.filesMaster[selectedFile];
   var filesTables = [];
+
   filesTables = filesArray[1];
+  console.log(filesTables , "files Tables");
+
   this.filesTables = filesTables['Transactions'];
    for (let i = 0; i < Object.keys(filesArray[0]).length; i++) {
     this.filesArray[i] = {
      key : Object.keys(filesArray[0])[i],
      value : Object.values(filesArray[0])[i]
-     
+
     };
   }
     console.log("Selected file",this.filesTables);
