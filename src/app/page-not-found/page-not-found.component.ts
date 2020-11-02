dd868d0568805ccc01d398d0272677361f30018c
Owais A. Khan <owais.khan@cr-pl.com>
2020-11-02 11:36:22 +0500

    ui-changes

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(public location : Location) {
+
+  }
 
   ngOnInit(): void {
   }
